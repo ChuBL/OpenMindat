ee0a1138d19e5913e9042c0b3de71e11398ecafe
ChuBL <wondered@msn.com>
2023-10-05 16:25:30 -0700

    added ima collector

--- OpenMindat/__init__.py
+++ OpenMindat/__init__.py
@@ -5,13 +5,14 @@ import json
 import re
 import pprint
 import requests
+from datetime import datetime
 
 
 def set_api_key(API_KEY):
-    # set api key and save it to a file
+    '''set api key and save it to a file'''
     mat = MindatApiTester()
 
-    if mat.test_api_key(API_KEY):
+    if mat.is_api_key_avail(API_KEY):
         mat.api_key = API_KEY
         with open('cached_api_key', 'w') as f:
             f.write(API_KEY)
@@ -19,16 +20,19 @@ def set_api_key(API_KEY):
     else:
         print("API key is invalid. Please check your API key. ")
 
+
 class MindatApiTester:
+    '''test if api key is valid
+    if valid, save it to a file'''
     def __init__(self) -> None:
-        self.api_key = None
+        pass
     
     def has_api_key(self):
         # check if api key is set
         try:
             with open('cached_api_key', 'r') as f:
                 api_key = f.read()
-            if self.test_api_key(api_key):
+            if self.is_api_key_avail(api_key):
                 self.api_key = api_key
                 return True
             else:
@@ -39,7 +43,7 @@ class MindatApiTester:
             return False
             
 
-    def test_api_key(self, API_KEY):
+    def is_api_key_avail(self, API_KEY):
         # test if api key is valid
         test_api_key = API_KEY
         MINDAT_API_URL = "https://api.mindat.org"
@@ -53,22 +57,21 @@ class MindatApiTester:
         else:
             return False
 
+
 class MindatApi:
+    '''The main class for openmindat API'''
     def __init__(self):
         mat = MindatApiTester()
         if False == mat.has_api_key():
-            print("Please set your API key using this function: \n\
-set_api_key('Your_API_Key'). ")
+            print("Please set your API key using this function: \nset_api_key('Your_API_Key'). ")
             return
         else:
             self._api_key = self.load_api_key()
         
-        print('successfully loaded API key. ')
-        
         self.MINDAT_API_URL = "https://api.mindat.org"
         self._headers = {'Authorization': 'Token '+ self._api_key}
         self.params = {'format': 'json'}
-        self.endpoint = "/items/"
+        # self.endpoint = "/items/"
         self.data_dir = './mindat_data/'
         Path(self.data_dir).mkdir(parents=True, exist_ok=True)
 
@@ -91,103 +94,104 @@ set_api_key('Your_API_Key'). ")
     def get_headers(self):
         return self._headers
     
-    def get_items(self, PARAMS_DICT = {}, FILENAME = 'mindat_items'):
-        if {} == PARAMS_DICT:
-            params = self.params
-        else:
-            params = PARAMS_DICT
-        print("Retrieving items with params:")
-        pprint.pprint(params)
+    # def get_items(self, PARAMS_DICT = {}, FILENAME = 'mindat_items'):
+    #     if {} == PARAMS_DICT:
+    #         params = self.params
+    #     else:
+    #         params = PARAMS_DICT
+    #     print("Retrieving items with params:")
+    #     pprint.pprint(params)
 
-        response = requests.get(self.MINDAT_API_URL+"/items/",
-                                params=params,
-                                headers=self._headers)
+    #     response = requests.get(self.MINDAT_API_URL+"/items/",
+    #                             params=params,
+    #                             headers=self._headers)
 
-        json_file = response.json()
-        if 'mindat_items' == FILENAME:
-            export_path = self.data_dir + 'mindat_items.json'
-        else:
-            export_path = self.data_dir + FILENAME + '.json'
+    #     json_file = response.json()
+    #     if 'mindat_items' == FILENAME:
+    #         export_path = self.data_dir + 'mindat_items.json'
+    #     else:
+    #         export_path = self.data_dir + FILENAME + '.json'
         
-        with open(export_path, 'w') as f:
-            json.dump(json_file, f, indent=4)
+    #     with open(export_path, 'w') as f:
+    #         json.dump(json_file, f, indent=4)
         
-        self.print_the_result(json_file, FILENAME)
+    #     self.print_the_result(json_file, FILENAME)
+        
 
     def print_the_result(self, JSONFILE, FILENAME):
         print("Successfully retrieved", len(JSONFILE['results']), "items in", FILENAME, '. ')
 
     
 
-    def get_all_items(self):
-        # https://api.mindat.org/items/?format=json
-        # return all mindat items (minerals, varieties, groups, synonyms, rocks, etc.),
-        # unfiltered, paginated
-        params = {'format': 'json',
-                'page_size': '20'}
-        response = requests.get(self.MINDAT_API_URL+"/items/",
-                                params=params,
-                                headers=self._headers, )
-        json_file = response.json()
-        saving_path = self.data_dir + 'mindat_items.json'
-        with open(saving_path, 'w') as f:
-            json.dump(json_file, f, indent=4)
+    # def get_all_items(self):
+    #     # https://api.mindat.org/items/?format=json
+    #     # return all mindat items (minerals, varieties, groups, synonyms, rocks, etc.),
+    #     # unfiltered, paginated
+    #     params = {'format': 'json',
+    #             'page_size': '20'}
+    #     response = requests.get(self.MINDAT_API_URL+"/items/",
+    #                             params=params,
+    #                             headers=self._headers, )
+    #     json_file = response.json()
+    #     saving_path = self.data_dir + 'mindat_items.json'
+    #     with open(saving_path, 'w') as f:
+    #         json.dump(json_file, f, indent=4)
 
-    def get_select_fields_items(self, FILEDS_STR = 'id,name,dispformulasimple',\
-                PAGE_SIZE = '100'):
-        # https://api.mindat.org/items/?fields=id,name,dispformulasimple&page_size=100
-        # display only selected fields.
-        # selecting only necessary fields slightly reduces db queries size so its appreciated
-        # customize page_size to 100 items per page
-        params = {'fields': FILEDS_STR,
-                'page_size': PAGE_SIZE,
-                'format': 'json'}
+    # def get_select_fields_items(self, FILEDS_STR = 'id,name,dispformulasimple',\
+    #             PAGE_SIZE = '100'):
+    #     # https://api.mindat.org/items/?fields=id,name,dispformulasimple&page_size=100
+    #     # display only selected fields.
+    #     # selecting only necessary fields slightly reduces db queries size so its appreciated
+    #     # customize page_size to 100 items per page
+    #     params = {'fields': FILEDS_STR,
+    #             'page_size': PAGE_SIZE,
+    #             'format': 'json'}
 
-        response = requests.get(self.MINDAT_API_URL+"/items/",
-                        params=params,
-                        headers=self._headers)
+    #     response = requests.get(self.MINDAT_API_URL+"/items/",
+    #                     params=params,
+    #                     headers=self._headers)
 
-        json_file = response.json()
-        field_sequence = re.sub(r'\,','_',FILEDS_STR)
-        saving_path = self.data_dir + 'mindat_items_with_fields_' + field_sequence + '.json'
-        with open(saving_path, 'w') as f:
-            json.dump(json_file, f, indent=4)
+    #     json_file = response.json()
+    #     field_sequence = re.sub(r'\,','_',FILEDS_STR)
+    #     saving_path = self.data_dir + 'mindat_items_with_fields_' + field_sequence + '.json'
+    #     with open(saving_path, 'w') as f:
+    #         json.dump(json_file, f, indent=4)
 
-    def get_omit_fields_items(self, OMIT_STR = 'id,name,dispformulasimple'):
-        # https://api.mindat.org/items/?omit=id,name,dispformulasimple
-        # exclude fields from display
-        params = {'omit': OMIT_STR,
-                'format': 'json'}
+    # def get_omit_fields_items(self, OMIT_STR = 'id,name,dispformulasimple'):
+    #     # https://api.mindat.org/items/?omit=id,name,dispformulasimple
+    #     # exclude fields from display
+    #     params = {'omit': OMIT_STR,
+    #             'format': 'json'}
 
-        response = requests.get(self.MINDAT_API_URL+"/items/",
-                        params=params,
-                        headers=self._headers)
+    #     response = requests.get(self.MINDAT_API_URL+"/items/",
+    #                     params=params,
+    #                     headers=self._headers)
 
-        json_file = response.json()
-        omit_sequence = re.sub(r'\,','_',OMIT_STR)
-        saving_path = self.data_dir + 'mindat_items_omit_fields_' + omit_sequence + '.json'
-        with open(saving_path, 'w') as f:
-            json.dump(json_file, f, indent=4)
+    #     json_file = response.json()
+    #     omit_sequence = re.sub(r'\,','_',OMIT_STR)
+    #     saving_path = self.data_dir + 'mindat_items_omit_fields_' + omit_sequence + '.json'
+    #     with open(saving_path, 'w') as f:
+    #         json.dump(json_file, f, indent=4)
 
-    def get_filtered_items(self, FILTERS_DICT = {'density__to': '3',
-          'crystal_system': 'Triclinic',
-          'color': 'red',
-          'ima': 1,          # show only minerals approved by ima
-          'format': 'json'}):
-        # for filters reference on this endpoint see generated documentation:
-        # https://api.mindat.org/schema/redoc/#tag/items/operation/items_list
+    # def get_filtered_items(self, FILTERS_DICT = {'density__to': '3',
+    #       'crystal_system': 'Triclinic',
+    #       'color': 'red',
+    #       'ima': 1,          # show only minerals approved by ima
+    #       'format': 'json'}):
+    #     # for filters reference on this endpoint see generated documentation:
+    #     # https://api.mindat.org/schema/redoc/#tag/items/operation/items_list
         
-        # filters on minerals, examples
-        # https://api.mindat.org/items/?density__to=3&crystal_system=Triclinic&color=red&ima=1
-        params = FILTERS_DICT
-        response = requests.get(self.MINDAT_API_URL+"/items/",
-                        params=params,
-                        headers=self._headers)
+    #     # filters on minerals, examples
+    #     # https://api.mindat.org/items/?density__to=3&crystal_system=Triclinic&color=red&ima=1
+    #     params = FILTERS_DICT
+    #     response = requests.get(self.MINDAT_API_URL+"/items/",
+    #                     params=params,
+    #                     headers=self._headers)
 
-        json_file = response.json()
-        saving_path = self.data_dir + 'mindat_items_filtered.json'
-        with open(saving_path, 'w') as f:
-            json.dump(json_file, f, indent=4)
+    #     json_file = response.json()
+    #     saving_path = self.data_dir + 'mindat_items_filtered.json'
+    #     with open(saving_path, 'w') as f:
+    #         json.dump(json_file, f, indent=4)
 
     def get_datetime(self):
         # use datetime to get current date and time
@@ -195,20 +199,16 @@ set_api_key('Your_API_Key'). ")
         dt_string = now.strftime("%m%d%Y%H%M%S")
         return dt_string
 
-    def get_ima_items(self):
+    def get_ima_minerals(self):
         '''
             get all minerals approved by ima.
             Since this API has a limit of 1000 items per page,
             we need to loop through all pages and save them to a single json file
         '''
-        # omit_str = '''updttime,varietyof,synid,polytypeof,groupid,
-        #     entrytype,entrytype_text,description_short,impurities
-        #     '''
-        ima_path = Path(self.data_dir, 'raw_data')
-        ima_path.mkdir(parents=True, exist_ok=True)
+   
         date = self.get_datetime()
         print("Retrieving Mindat data for IMA approved minerals. This may take a while... ")
-        file_path = Path(ima_path, 'mindat_items_IMA_' + date + '.json')
+        file_path = Path(self.data_dir, 'ima_' + date + '.json')
         with open(file_path, 'w') as f:
 
             params = {
@@ -217,6 +217,7 @@ set_api_key('Your_API_Key'). ")
                 "ima_status": [
                         "APPROVED"
                     ],
+                'page_size': '1500',
                 'format': 'json'
             }
 
@@ -235,11 +236,9 @@ set_api_key('Your_API_Key'). ")
 
                 except requests.exceptions.MissingSchema as e:
                     # This error indicates the `next_url` is none
+                    # i.e., we've reached the end of the results
                     break
-            # last_count = self.ima_last_count_check(ima_path)
-            # if last_count > len(json_all['results']):
-            #     print('IMA approved minerals retrieving failed. Exiting...')
-            #     return
+
             json.dump(json_data, f, indent=4)
         print("Successfully saved " + str(len(json_data['results'])) + " entries to " + str(file_path))
 
